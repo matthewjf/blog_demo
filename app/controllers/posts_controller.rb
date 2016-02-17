82d16230f90fb896b50b55a290ee0b6f50a19588
matthew <matthewjf@gmail.com>
2016-02-16 17:12:01 -0800

    some changes (bad at git)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
-  before_action :logged_in_user, except: [:index, :show]
   before_action :admin_user, except: [:index, :show]
+  before_action :logged_in_user, except: [:index, :show]
 
   def index
     @posts = Post.paginate(page: params[:page])
--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class PostsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @post = posts(:orange)
+  end
+
+  test "should redirect create when not admin" do
+    assert_no_difference 'Post.count' do
+      post :create, micropost: { title: "Lorem ipsum"}
+    end
+    assert_redirected_to root_url
+  end
+
+  test "should redirect destroy when not admin" do
+    assert_no_difference 'Post.count' do
+      delete :destroy, id: @post
+    end
+    assert_redirected_to root_url
+  end
 end
