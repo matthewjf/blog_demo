20478db0d4ec2bf33c26c763b104d1761fd3625d
matthew <matthewjf@gmail.com>
2016-03-01 20:47:31 -0800

    remove comments from unpublished posts

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,6 @@
 class CommentsController < ApplicationController
   before_action :logged_in_user, only: :create
+  before_action :post_published, only: :create
 
   def index
     @post = Post.find(params[:post_id])
@@ -27,5 +28,13 @@ class CommentsController < ApplicationController
   private
     def comment_params
       params.require(:comment).permit(:body)
+    end
+
+    def post_published
+      post = Post.find(params[:post_id])
+      unless post.published
+        flash[:error] = "Publish the post before adding comments"
+        redirect_to post
+      end
     end
 end
